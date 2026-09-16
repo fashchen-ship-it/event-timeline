1bb18ef6593a67ea280a8059cb926340b86aec92
yuya <fashchen@gmail.com>
2026-09-16 21:30:58 +0800

    fix: clean companion background and balance pets

--- src/components/ui/pixel-companions.tsx
+++ src/components/ui/pixel-companions.tsx
@@ -13,7 +13,7 @@ export function PixelCompanionShelf() {
         </div>
       </div>
       <div aria-hidden="true" className="pixel-companion-art">
-        <Image alt="" className="pixel-companion-cat" height={1280} src="/companions/tabby-logbook.png" width={1280} />
+        <Image alt="" className="pixel-companion-cat" height={1280} src="/companions/tabby-logbook-clean.png" width={1280} />
         <Image alt="" className="pixel-companion-dog" height={1280} src="/companions/poodle-note.png" width={1280} />
       </div>
     </aside>
